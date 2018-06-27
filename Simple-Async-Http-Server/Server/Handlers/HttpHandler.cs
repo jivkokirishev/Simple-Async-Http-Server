139a740233a1d6394677062971c8499330c5cb8c
jivkokirishev <jivkokirishev@gmail.com>
2018-06-27 23:24:19 +0300

    Http handler created which connects the routes with the request handling (request response logic)

---
+++ Simple-Async-Http-Server/Server/Handlers/HttpHandler.cs
@@ -0,0 +1,52 @@
+﻿using Simple_Async_Http_Server.Server.Handlers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple_Async_Http_Server.Server.Http.Contracts;
+using Simple_Async_Http_Server.Server.Routing.Contracts;
+using Simple_Async_Http_Server.Server.Common;
+using System.Text.RegularExpressions;
+using Simple_Async_Http_Server.Server.Http.Response;
+
+namespace Simple_Async_Http_Server.Server.Handlers
+{
+    public class HttpHandler : IRequestHandler
+    {
+        private readonly IServerRouteConfig routeConfig;
+
+        public HttpHandler(IServerRouteConfig routeConfig)
+        {
+            CommonValidator.ThrowIfNull(routeConfig, nameof(routeConfig));
+
+            this.routeConfig = routeConfig;
+        }
+
+        public IHttpResponse Handle(IHttpContext httpContext)
+        {
+            CommonValidator.ThrowIfNull(httpContext, nameof(httpContext));
+
+            var method = httpContext.Request.RequestMethod;
+            foreach (var route in this.routeConfig.Routes[method])
+            {
+                var pattern = route.Key;
+                var routeRegex = new Regex(pattern);
+                var match = routeRegex.Match(httpContext.Request.Path);
+
+                if (match.Success)
+                {
+                    foreach (var parameter in route.Value.Parameters)
+                    {
+                        var paramValue = match.Groups[parameter].Value;
+                        httpContext.Request.AddUrlParameter(parameter, paramValue);
+                    }
+
+                    return route.Value.RequestHandler.Handle(httpContext);
+                }
+            }
+
+            return new NotFoundResponse();
+        }
+    }
+}
---
+++ Simple-Async-Http-Server/Server/Http/Response/NotFoundResponse.cs
@@ -0,0 +1,19 @@
+﻿using Simple_Async_Http_Server.Server.Enums;
+using Simple_Async_Http_Server.Server.Http.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Async_Http_Server.Server.Http.Response
+{
+    public class NotFoundResponse : HttpResponse
+    {
+        public NotFoundResponse()
+            : base()
+        {
+            this.StatusCode = HttpStatusCode.NotFound;
+        }
+    }
+}
